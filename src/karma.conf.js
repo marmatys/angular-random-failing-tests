00880edd8223eef53e447bac5f4fc74467edcb35
Marcin Armatys <marcin.armatys@gmail.com>
2019-01-21 23:19:37 +0100

    Adding specific seed for jasmine in karma.conf.js
    In this way we can have failing test for every run.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -19,7 +19,8 @@ module.exports = function (config) {
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       jasmine: {
-        random: true
+        random: true,
+        seed: 94349
       }
     },
     coverageIstanbulReporter: {
